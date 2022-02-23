0386c39402713d30a119e63c1d2d37ef809ff9fa
Dannyzn <694518723@qq.com>
2022-02-23 11:46:44 +0800

    add-cors-cookie-try

--- src/app2.js
+++ src/app2.js
@@ -3,9 +3,15 @@ const express = require('express')
 const app = express()
 
 // 定义一个接口  index.html  页面请求这个接口的 就是 跨域 (因为端口不同)
-app.get('/anotherService', (req, res) => {
-    res.json({ code: 0, msg: "这是8003 端口返回的"})
-})
+// app.get('/anotherService', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:8003");
+//     res.json({ code: 0, msg: "这是8003 端口返回的"})
+// })
+app.all("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+    res.header("Access-Control-Allow-Credentials", "true"); // ++ 新增
+    next();
+});
 
 app.listen("8003", () => {
     console.log("app2 running at port 8003");
